60f0e572e43fceca6187b70263387fa334c083b7
Habib Khan <158816354+habibkhan004@users.noreply.github.com>
2024-04-12 14:19:21 +0500

    initial commit

---
+++ alert.js
@@ -0,0 +1,10 @@
+let age=prompt("Please enter your age:");
+if(age>18)
+{
+    console.log("you are Allowed to enter the club");
+    alert("you are allowed to enter the club");
+}
+else{
+    console.log("you are not allowed to enter the club");
+    alert("You are not allowed to enter the club");
+}
